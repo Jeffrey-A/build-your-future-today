a043a66d55b1735131ab8e69d346495bc1b002f9
Jeffrey-A <almanzarortizjeffrey@gmail.com>
2021-01-19 22:57:31 -0500

    fix(*): add front-end build folder so that it can be served on heroku

---
+++ front_end/build/precache-manifest.4b3180b893470c0969f41cb90877cb3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72cf3b91fcca78078310a75cf2526924",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eebda13862ceb3683bb5",
+    "url": "/static/css/main.4164cc20.chunk.css"
+  },
+  {
+    "revision": "47ebdfa7671d7ea9e082",
+    "url": "/static/js/2.e0d39bd1.chunk.js"
+  },
+  {
+    "revision": "eebda13862ceb3683bb5",
+    "url": "/static/js/main.5f69a6e9.chunk.js"
+  },
+  {
+    "revision": "27494562e1dda3b9f1b7",
+    "url": "/static/js/runtime-main.d5382737.js"
+  },
+  {
+    "revision": "cf174db6f21e0db0e194b17fcbbde485",
+    "url": "/static/media/home.cf174db6.png"
+  }
+]);
